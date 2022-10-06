56a757c247e07a74cf803eb5880c81d96b16aa45
Emrecanelma <emrecan_apple_45@hotmail.com>
2022-10-06 21:31:24 +0300

    go
    golang basic ex

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+// Import statement
+import "fmt"
+
+// My Code
+func main() {
+
+	name := "Arin"
+	age := 40
+	isMarried := true
+
+	age = 41
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
